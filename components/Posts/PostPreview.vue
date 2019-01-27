1877b0366b35e0af7a88d37c5acf85ebd0a0ad1f
Macbook <macbook@Macbooks-MacBook-Pro-3.local>
2019-01-26 20:32:29 -0500

    Spliting the page into components

---
+++ components/Posts/PostPreview.vue
@@ -0,0 +1,62 @@
+<template>
+  <nuxt-link
+    :to="'/posts/' + id"
+    class="app-post-link-wrap"
+  >
+    <article class="post-preview">
+      <div
+        :style="{backgroundImage: 'url(' + thumbnail + ')'}"
+        class="post-thumbnail"
+      >
+        <!-- ESLint not allow empty tags -->
+      </div>
+      <div class="post-content">
+        <h3>{{ title }}</h3>
+        <p>{{ previewText }}</p>
+      </div>
+    </article>
+  </nuxt-link>
+</template>
+
+<script>
+  export default {
+    name: "PostPreview",
+    props: {
+      id: {
+        type: String,
+        required: true
+      },
+      title: {
+        type: String,
+        required: true
+      },
+      previewText: {
+        type: String,
+        required: true
+      },
+      thumbnail: {
+        type: String,
+        required: true
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .app-post-link-wrap {
+    display: block;
+    padding: 0 10px 10px;
+    margin: 20px 0;
+    box-shadow: 0 .5rem 1rem rgba(0,0,0,.15);
+    text-decoration: none;
+  }
+
+  .post-thumbnail {
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center;
+    height: 300px;
+    width: 100%;
+    margin-bottom: 20px;
+  }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -6,59 +6,35 @@
     <section class="featured-posts">
       <div class="row">
         <div class="col-sm-12">
-          <nuxt-link
-            :to="'/posts/' + 1"
-            class="app-post-link-wrap"
-          >
-            <article class="post-preview">
-              <div
-                class="post-thumbnail"
-                style="background-image: url('https://www.eatwell101.com/wp-content/uploads/2017/03/baked-Garlic-Butter-Chicken-recipe.jpg')"
-              >
-                <!-- ESLint not allow empty tags -->
-              </div>
-              <div class="post-content">
-                <h3>Post Title 1</h3>
-                <p>Preview Text 1</p>
-              </div>
-            </article>
-          </nuxt-link>
 
-          <nuxt-link
-            :to="'/posts/' + 2"
-            class="app-post-link-wrap"
-          >
-            <article class="post-preview">
-              <div
-                class="post-thumbnail"
-                style="background-image: url('https://realfood.tesco.com/media/images/RFO-HoneyGlazedHam-1400x919-copy-1a07a533-a3a5-4b25-90c1-29e24aa000f5-0-1400x919.jpg')"
-              >
-                <!-- ESLint not allow empty tags -->
-              </div>
-              <div class="post-content">
-                <h3>Post Title 2</h3>
-                <p>Preview Text 2</p>
-              </div>
-            </article>
-          </nuxt-link>
+          <PostPreview
+            id="1"
+            title="Post title 1"
+            preview-text="Description for post 1"
+            thumbnail="https://www.eatwell101.com/wp-content/uploads/2017/03/baked-Garlic-Butter-Chicken-recipe.jpg"
+          />
 
-          <nuxt-link
-            :to="'/posts/' + 3"
-            class="app-post-link-wrap"
-          >
-            <article class="post-preview">
-              <div
-                class="post-thumbnail"
-                style="background-image: url('http://cdn.recipes100.com/v/95df700ca7c4db714cdeb7285bad8896.jpg')"
-              >
-                <!-- ESLint not allow empty tags -->
-              </div>
-              <div class="post-content">
-                <h3>Post Title 3</h3>
-                <p>Preview Text 3</p>
-              </div>
-            </article>
-          </nuxt-link>
+          <PostPreview
+            id="2"
+            title="Post title 2"
+            preview-text="Description for post 2"
+            thumbnail="https://realfood.tesco.com/media/images/RFO-HoneyGlazedHam-1400x919-copy-1a07a533-a3a5-4b25-90c1-29e24aa000f5-0-1400x919.jpg"
+          />
+
+          <PostPreview
+            id="3"
+            title="Post title 3"
+            preview-text="Description for post 3"
+            thumbnail="http://cdn.recipes100.com/v/95df700ca7c4db714cdeb7285bad8896.jpg"
+          />
+
+          <PostPreview
+            id="4"
+            title="Post title 4"
+            preview-text="Description for post 4"
+            thumbnail="https://www.eatwell101.com/wp-content/uploads/2017/03/baked-Garlic-Butter-Chicken-recipe.jpg"
+          />
+
         </div>
       </div>
     </section>
@@ -66,28 +42,16 @@
 </template>
 
 <script>
+import PostPreview from '@/components/Posts/PostPreview';
 
 export default {
-
+  components: {
+    PostPreview
+  }
 }
 
 </script>
-<style lang="scss" scoped>
-  .app-post-link-wrap {
-    display: block;
-    padding: 0 10px 10px;
-    margin: 20px 0;
-    box-shadow: 0 .5rem 1rem rgba(0,0,0,.15);
-    text-decoration: none;
-  }
 
-  .post-thumbnail {
-    background-size: cover;
-    background-repeat: no-repeat;
-    background-position: center;
-    height: 300px;
-    width: 100%;
-    margin-bottom: 20px;
-  }
+<style lang="scss" scoped>
 
 </style>
